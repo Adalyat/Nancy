434c40152cbdcece76776377d98ffa331fb54a65
Adalyat Nazirov <aenazirov@gmail.com>
2014-02-04 10:12:02 +0400

    add mini profiler library

---
+++ src/Nancy.Demo.MiniProfiler/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.TinyIoc;
+using Nancy.Bootstrapper;
+using StackExchange.Profiling;
+
+namespace NancyViewStart
+{
+    public class Bootstrapper:DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            pipelines.BeforeRequest += PreRequest;
+            pipelines.AfterRequest += PostRequest;
+        }
+
+        private static Response PreRequest(NancyContext ctx)
+        {
+            MiniProfiler.Start();
+            return null;
+        }
+
+        private static void PostRequest(NancyContext ctx)
+        {
+            //MiniProfiler.Stop();
+        }
+    }
+}
---
+++ src/Nancy.Demo.MiniProfiler/Modules/HomeModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using StackExchange.Profiling;
+
+namespace NancyViewStart.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+            : base("/")
+        {
+            Get["/"] = _ =>
+            {
+                using (MiniProfiler.StepStatic("Indexing"))
+                {
+                    return View["index"];
+                }
+            };
+        }
+    }
+}
